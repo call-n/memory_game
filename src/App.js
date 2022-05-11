a912c46f9f70d14a8ccf513c06003b2dabf4ef1c
call-n <callenilsson2001@gmail.com>
2022-05-11 10:56:41 +0200

    Shuffle cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { useState } from 'react';
+
+const cardImages = [
+    { "src": "/img/helmet-1.png" },
+    { "src": "/img/potion-1.png" },
+    { "src": "/img/ring-1.png" },
+    { "src": "/img/scroll-1.png" },
+    { "src": "/img/shield-1.png" },
+    { "src": "/img/sword-1.png" },
+]
 
 function App() {
+  const [cards, setCards] = useState([])
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+      setCards(shuffledCards)
+  }
+
+  console.log(cards)
+
   return (
     <div className="App">
-      lol
+      <button onClick={shuffleCards} >Restart Game</button>
     </div>
   );
 }
